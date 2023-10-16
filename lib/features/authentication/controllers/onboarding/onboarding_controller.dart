fe1e94583730ad9f91fdf28fe3d20b5aa3ccc695
alansomathew <alansomathew10@gmail.com>
2023-10-16 16:04:05 +0530

    first commit and basic ui screens

---
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_app/features/authentication/screens/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  // variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  // update current index wwhen page scrolls
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+
+  // jumb to spectfic dot selected page
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpToPage(index);
+  }
+
+  // update current index and jumb to next page
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      Get.to(const LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  // update curent inex and jumb to last page
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+}
---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:ecommerce_app/features/authentication/controllers/onboarding/onboarding_controller.dart';
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_navigation.dart';
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_nextbutton.dart';
+import 'package:ecommerce_app/utils/constants/text_strings.dart';
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
+   
+    return Scaffold(
+      body: Stack(
+        children: [
+          // horizontal scrollable pages
+          PageView(
+            controller: controller.pageController,
+            onPageChanged: controller.updatePageIndicator,
+            children: const [
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+
+          // skip button
+          const OnBoardingSkip(),
+
+          // bot navigation smooth page indicator
+          const OnBoardingNavigation(),
+
+          // circular button
+          const OnBoardingNextButton(),
+        ],
+      ),
+    );
+  }
+}
