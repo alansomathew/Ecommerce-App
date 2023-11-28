bb04c41c618ece9a7a6a436fa383b26c8a5161db
alansomathew <alansomathew10@gmail.com>
2023-11-28 15:23:41 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp( const MyApp());
+//* Entry point of flutter App
+Future<void> main() async {
+// Todo: Add Widgets Binding
+// Todo: Init local storage
+// Todo: Init Payment Methods
+// Todo: Await Native Splash
+// Todo: Initialize Firebase
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+// Todo: Initialize Authentication
+
+// Todo: Load all the Material Design / Themes / Localizations / Bindings
+  runApp(const MyApp());
 }
